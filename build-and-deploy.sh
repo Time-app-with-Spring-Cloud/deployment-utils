aaec797db2873ee8e6821aa412702074ff611189
Pedro Galan <pedro.galan@piksel.com>
2020-06-09 21:50:25 +0100

    Include the new container in docker-compose

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -2,6 +2,7 @@
 
 # Create the Docker images of all the microservices
 cd ../time-service && mvn clean install && cd -
+cd ../time-eureka-server && mvn clean install && cd -
 
 # Start the Docker containers of all the microservices
 cd docker && docker-compose up
