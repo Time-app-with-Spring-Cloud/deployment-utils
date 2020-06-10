21f399489d00dacdeb1a13877c112b7ac40948fd
Pedro Galan <pedro.galan@piksel.com>
2020-06-10 10:47:50 +0100

    Make coming back to the original directory more robust

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+CURRENT_DIR=`pwd`
+
 # Create the Docker images of all the microservices
-cd ../time-service && mvn clean install && cd -
-cd ../time-eureka-server && mvn clean install && cd -
+cd ../time-service && mvn clean install && cd $CURRENT_DIR
+cd ../time-eureka-server && mvn clean install && cd $CURRENT_DIR
 
 # Start the Docker containers of all the microservices
 cd docker && docker-compose up
