ba176a80dfd849f2db819b2f199c5ea4552d59e8
Pedro Galan <pedro.galan@piksel.com>
2020-06-10 15:31:56 +0100

    Clean up

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -3,9 +3,9 @@
 CURRENT_DIR=`pwd`
 
 # Create the Docker images of all the microservices
-cd ../time-service && mvn clean install && cd $CURRENT_DIR
-cd ../time-eureka-server && mvn clean install && cd $CURRENT_DIR
-cd ../time-config-server && mvn clean install && cd $CURRENT_DIR
+cd ../time-service       && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
+cd ../time-eureka-server && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
+cd ../time-config-server && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
 
 # Start the Docker containers of all the microservices
 cd docker && docker-compose up
