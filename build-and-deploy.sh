dc19c549233b6c086b811ebf62182ba3df2cab12
Pedro Galan <pedro.galan@piksel.com>
2020-06-12 15:05:48 +0100

    Add Hystrix dashboard

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -3,10 +3,11 @@
 CURRENT_DIR=`pwd`
 
 # Create the Docker images of all the microservices
-cd ../time-service       && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
-cd ../time-eureka-server && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
-cd ../time-config-server && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
-cd ../time-config-ui     && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
+cd ../time-service           && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
+cd ../time-eureka-server     && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
+cd ../time-config-server     && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
+cd ../time-ui                && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
+cd ../time-hystrix-dashboard && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
 
 # Start the Docker containers of all the microservices
 cd docker && docker-compose up --scale time-service=2
