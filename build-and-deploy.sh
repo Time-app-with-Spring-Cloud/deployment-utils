296f78b5d4daf3278b4ecb30dff9b9b0b7750343
Pedro Galan <pedro.galan@piksel.com>
2020-06-15 12:59:25 +0100

    Add new services related to messaging

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -8,6 +8,7 @@ cd ../time-eureka-server     && mvn clean install -Dmaven.test.skip=true && cd $
 cd ../time-config-server     && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
 cd ../time-ui                && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
 cd ../time-hystrix-dashboard && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
+cd ../time-intake            && mvn clean install -Dmaven.test.skip=true && cd $CURRENT_DIR
 
 # Start the Docker containers of all the microservices
 cd docker && docker-compose up --scale time-service=2
